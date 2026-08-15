6428d97e17114f0d8494ad3eedd5f9dae8be2798
jbalboaU <juan.balboa@ug.uchile.cl>
2026-08-15 10:02:36 -0400

    feat: add Soldier constructor

--- src/main/scala/soldier/Soldier.scala
+++ src/main/scala/soldier/Soldier.scala
@@ -1,5 +1,8 @@
 package soldier
 
-class Soldier {
+class Soldier (var hp: Int, val name: String, val attack: Int, private var isAlive: Boolean = true){
+  // En el constructor, la variable isAlive es private
+  // dado que solo se utiliza dentro de la misma clase.
+  // Es decir, solo Soldier la modifica.
 
 }
