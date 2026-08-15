c04c453c5867c8d8f4e1b3f93d35acf3e18a1b8d
jbalboaU <juan.balboa@ug.uchile.cl>
2026-08-15 10:04:57 -0400

    feat: add the Soldier field

--- src/main/scala/soldier/Soldier.scala
+++ src/main/scala/soldier/Soldier.scala
@@ -1,8 +1,11 @@
 package soldier
 
+import army.Army
+
 class Soldier (var hp: Int, val name: String, val attack: Int, private var isAlive: Boolean = true){
   // En el constructor, la variable isAlive es private
   // dado que solo se utiliza dentro de la misma clase.
   // Es decir, solo Soldier la modifica.
 
+  var army: Army = null
 }
