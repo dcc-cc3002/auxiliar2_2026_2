dd48b9e9ef12423f086250109180b5dc3e66d21d
jbalboaU <juan.balboa@ug.uchile.cl>
2026-08-15 10:06:27 -0400

    docs: add the documentation for the Soldier class

--- src/main/scala/soldier/Soldier.scala
+++ src/main/scala/soldier/Soldier.scala
@@ -2,6 +2,19 @@ package soldier
 
 import army.Army
 
+
+/**
+ * Represents a soldier that can belong to an army and engage in combat.
+ *
+ * A soldier has health points that decrease when attacked. When hp drops
+ * to 0 or below, the soldier dies and automatically leaves their army.
+ * A soldier can belong to at most one army at a time.
+ *
+ * @param hp        the health points of the soldier
+ * @param name      the name of the soldier
+ * @param attack    the damage inflicted on another soldier per attack
+ * @param isAlive     the value that indicates whether a soldier is alive or dead
+ */
 class Soldier (var hp: Int, val name: String, val attack: Int, private var isAlive: Boolean = true){
   // En el constructor, la variable isAlive es private
   // dado que solo se utiliza dentro de la misma clase.
