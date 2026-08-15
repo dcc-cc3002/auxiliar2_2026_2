9d4859b8a898ead0ee88adaf9d6c7d8b598ec11e
jbalboaU <juan.balboa@ug.uchile.cl>
2026-08-15 10:14:56 -0400

    feat: implement methods for Soldier

--- src/main/scala/soldier/Soldier.scala
+++ src/main/scala/soldier/Soldier.scala
@@ -23,19 +23,34 @@ class Soldier (var hp: Int, val name: String, val attack: Int, private var isAli
   var army: Army = null
 
   def attackSoldier(soldier: Soldier): Unit = {
-
+    val newHp: Int = soldier.hp - attack
+    if (newHp <= 0) {
+      soldier.hp = 0
+      soldier.die()
+    } else {
+      soldier.hp = newHp
+    }
   }
 
   def joinArmy(newArmy: Army): Unit = {
-
+    if (army != null) {
+      this.leaveArmy()
+    }
+    newArmy.join(this)
+    army = newArmy
   }
 
   def leaveArmy(): Unit = {
-
+    if (this.army != null) {
+      army.leave(this)
+      army = null
+    }
   }
 
   private def die(): Unit = {
     // Este método también es privado, dado que se utiliza
     //  únicamente dentro de la clase Soldier.
+    isAlive = false
+    this.leaveArmy()
   }
 }
